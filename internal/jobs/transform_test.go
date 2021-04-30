58dd39b2751732498ab8bc3df92cdbd8d2f1fccd
Bjørn Cintra <bjorn.tore.cintra@inmeta.no>
2021-04-30 10:06:37 +0200

    [FEATURE] Update transform functions #36
    Some of the transform functions were a little bit wonky, and some useful
    functions were also missing.
    
    All JavaScript functions have been updated to prevent erroring when missing
    values, either entities or fields or properties. Functions returning values
    are now all returning undefined if no value can be found, rather than a mix
    of null and undefined (this should have no impact, as before they would
    just crash).
    
     * SetProperty(entity, prefix, name, value) - no longer crash
     * GetProperty(entity, prefix, name, defaultValue) - no longer crash, optional default value
     * AddReference(entity, prefix, name, value) - no longer crash
     * GetId(entity) - no longer crash
     * SetId(entity, id) - new
     * SetDeleted(entity, deleted) - new
     * GetDeleted(entity) - new
     * RenameProperty(entity, originalPrefix, originalName, newPrefix, newName) - no crash
     * RemoveProperty(entity, prefix, name) - no longer crash
    
    In addition, the ToString function has been moved from JavaScript to Go,
    this was done to be able to correctly convert Entities to string. (no longer
    just [object Object]).
    Also the ToString function will now return "undefined" is the underlying
    object is a Go nil. Since Go makes no difference of undefined vs null, I
    chose to use undefined in the logs instead of null. The user must still check
    for null or undefined.
    
    This feature is accompanied with a matching change in the Mim CLI project.

--- internal/jobs/transform_test.go
+++ internal/jobs/transform_test.go
@@ -38,3 +38,54 @@ func TestTransform(t *testing.T) {
 		})
 	})
 }
+
+func TestJavascriptTransform_ToString(t *testing.T) {
+	g := goblin.Goblin(t)
+	g.Describe("Calling ToString from a transform", func() {
+		g.It("should return null when nil", func() {
+			transform := &JavascriptTransform{}
+			ret := transform.ToString(nil)
+
+			g.Assert(ret).Equal("undefined")
+		})
+		g.It("number should be formatted as a number", func() {
+			transform := &JavascriptTransform{}
+			ret := transform.ToString(30)
+			g.Assert(ret).Equal("30")
+
+			ret = transform.ToString(2.1)
+			g.Assert(ret).Equal("2.1")
+		})
+		g.It("bool should be formatted with true or false", func() {
+			transform := &JavascriptTransform{}
+			ret := transform.ToString(true)
+			g.Assert(ret).Equal("true")
+		})
+		g.It("a map should correctly formatted", func() {
+			transform := &JavascriptTransform{}
+
+			lemap := map[string]interface{}{
+				"field1": 1,
+				"field2": "2",
+			}
+
+			ret := transform.ToString(lemap)
+			g.Assert(ret).Equal("map[field1:1 field2:2]")
+
+		})
+		g.It("an entity should be formatted correctly", func() {
+			transform := &JavascriptTransform{}
+
+			e := transform.NewEntity()
+			e.ID = "ns1:1"
+			e.InternalID = 1
+			e.Recorded = 2
+			e.Properties = map[string]interface{}{
+				"field1": "hello",
+			}
+			ret := transform.ToString(e)
+
+			g.Assert(ret).Equal("&{ns1:1 1 2 false map[] map[field1:hello]}")
+		})
+	})
+}
