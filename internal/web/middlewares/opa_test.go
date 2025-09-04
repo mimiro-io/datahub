5d327400b32844181f4f7f6b8174be99a71f76f3
shinlim <shin.lim@mimiro.no>
2025-09-04 11:52:59 +0200

    Resolve lead developer PR feedback

--- internal/web/middlewares/opa_test.go
+++ internal/web/middlewares/opa_test.go
@@ -21,7 +21,7 @@ import (
 	"go.uber.org/zap"
 )
 
-func Test_parse_mim_decision_admin(t *testing.T) {
+func Test_parse_wildcard(t *testing.T) {
 	result := []byte("{\"decision_id\":\"5c78dda3-6389-4fc3-9f01-764f5ef9fe36\",\"result\":{\"*\":true}}")
 	logger := zap.NewNop().Sugar()
 	ds, err := parseDatasetsFromOpaBody(logger, result)
@@ -39,8 +39,8 @@ func Test_parse_mim_decision_admin(t *testing.T) {
 	}
 }
 
-func Test_parse_mim_decision_datasets(t *testing.T) {
-	result := []byte("{\"decision_id\":\"5c78dda3-6389-4fc3-9f01-764f5ef9fe36\",\"result\":[\"cima.AnimalBirthEvent\", \"cima.AnimalDeathEvent\"]}")
+func Test_parse_slice_datasets(t *testing.T) {
+	result := []byte("{\"decision_id\":\"5c78dda3-6389-4fc3-9f01-764f5ef9fe36\",\"result\":[\"TestEvent1\", \"TestEvent2\"]}")
 	logger := zap.NewNop().Sugar()
 	ds, err := parseDatasetsFromOpaBody(logger, result)
 
@@ -50,11 +50,11 @@ func Test_parse_mim_decision_datasets(t *testing.T) {
 	if len(ds) != 2 {
 		t.Fatalf("should have 2 datasets : %+v", ds)
 	}
-	if !slices.Contains(ds, "cima.AnimalBirthEvent") {
-		t.Fatalf("should have cima.AnimalBirthEvent dataset : %+v", ds)
+	if !slices.Contains(ds, "TestEvent1") {
+		t.Fatalf("should have TestEvent1 dataset : %+v", ds)
 	}
-	if !slices.Contains(ds, "cima.AnimalDeathEvent") {
-		t.Fatalf("should have cima.AnimalDeathEvent dataset : %+v", ds)
+	if !slices.Contains(ds, "TestEvent2") {
+		t.Fatalf("should have TestEvent2 dataset : %+v", ds)
 	}
 }
 
